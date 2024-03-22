0d7a0c95bd172bba1132d2ca7a909bd479626853
Varun Kumar <varunkumar63931@gmail.com>
2024-03-22 09:14:04 +0530

    backend

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,12 +1,51 @@
 const express = require('express');
 const router =express.Router();
+const Model= require('../models/postModels')
 
-router.get('/add',(req,res) => {
-    res.send('post add operation');
+
+
+router.post('/add',(req,res) => {
+    console.log(req.body);
+    
+  //to save data in mongodb
+    new Model(req.body).save()
+    
+    .then((result)=> {
+        res.status(200).json(result);
+    }).catch((err)=> {
+        res.status(500).json(err);
+    });
+    
+    
+});
+
+Model.find()
+.then((result) => {
+    res.status(200).json(result);
+}).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+});
+
+// :denotes url parameter
+router.delete('/delete/:id',(req ,res)=> {
+    Model.findByIdAndDelete(req.params.id)
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 })
 
-router.get('/getall',(req,res) => {
-    res.send('post read operation');
-});
+router.put('/update/:id', (req,res)=> {
+    Model.findByIdAndUpdate(req.params.id, req.body)
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
 
 module.exports =router;
